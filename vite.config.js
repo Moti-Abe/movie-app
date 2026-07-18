0911a4edbac5bbc58e2ca01a476ff544bca63629
Moti-Abe <motiabe8@gmail.com>
2026-07-18 12:29:59 +0300

    Deployment set up

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // use Vite's import.meta.env instead of process.env
+  base: import.meta.env.VITE_BASE_PATH || '/movie-app',
 })
